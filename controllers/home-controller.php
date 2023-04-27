ed778263f8b0842d33df5e71c280b4bb869c177d
henri goret <henrigoret@hotmail.com>
2023-04-27 16:23:59 +0200

    date ?

--- controllers/home-controller.php
+++ controllers/home-controller.php
@@ -1,5 +1,8 @@
 
 <?php 
+require_once __DIR__. '/../helpers/dd.php';
 
+include __DIR__. '/../views/templates/header.php';
 include __DIR__. '/../views/home.php';
+include __DIR__. '/../views/templates/footer.php';
 
